8d23c6e3d90de8460fd50e1fbbc1e9781f372f58
sorin800 <sorintruica@gmail.com>
2022-06-30 23:53:17 +0300

    first commit

---
+++ src/main/java/com/luv2code/springmvc/repository/StudentDao.java
@@ -0,0 +1,14 @@
+package com.luv2code.springmvc.repository;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface StudentDao extends CrudRepository<CollegeStudent, Integer> {
+
+    CollegeStudent findByEmailAddress(String emailAddress);
+
+}
---
+++ src/main/resources/insertData.sql
@@ -0,0 +1,4 @@
+insert into student(id, firstname,lastname,email_address) values (11, 'Student', 'One', 'student.one@luv2code.com')
+insert into student(id, firstname,lastname,email_address) values (12, 'Student', 'Second', 'student.second@luv2code.com')
+insert into student(id, firstname,lastname,email_address) values (13, 'Student', 'Third', 'student.third@luv2code.com')
+insert into student(id, firstname,lastname,email_address) values (14, 'Student', 'Fourth', 'student.fourth@luv2code.com')
